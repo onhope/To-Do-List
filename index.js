f612edd5a3d705b1829c1fc5216c71744cbe3981
onhope <chch4934@gmail.com>
2023-11-02 14:26:59 +0900

    To Do List

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const inputEl = document.querySelector(".input");
 const ulEl = document.querySelector(".list");
 let list = JSON.parse(localStorage.getItem("list"));
 
-list.forEach(task => {
-  todoList(task);
-});
+if (list) {
+  list.forEach((task) => {
+    todoList(task);
+  });
+}
 
-formEl.addEventListener("submit", (e) => {
+formEl.addEventListener("submit", (event) => {
   event.preventDefault();
   todoList();
 });
